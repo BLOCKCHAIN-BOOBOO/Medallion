ff38789c10ed7304307f6dca247eb379c68a7e99
Himani Bondalapati <himanidtpv@gmail.com>
2023-01-20 21:08:01 +0530

    design changes

--- src/components/MedallionDisplayCard/BasicData.jsx
+++ src/components/MedallionDisplayCard/BasicData.jsx
@@ -17,11 +17,24 @@ function BasicData(props) {
         // />
        <div className="" style={{padding:"30px"}}>
         <div className="rounded-full bg-slate-300 w-full" 
-         medallion_name={props.medallion.name}  medallion_elite_status={props.medallion.elite_status}>
+          medallion_elite_status={props.medallion.elite_status}>
+          {/* <span className="text-md" >{props.medallion.name}</span> */}
+          
+         
 
-        <div className="rounded-full self-center text-center h-full bg-yellow-400">
+          <svg viewBox="-5 0 130 130">
+  <path id="MyPath" fill="none" stroke="lightblue"
+        d="m10,45c53,-33 96,3 96,3" />
+        {/* <path id="MyPath" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" /> */}
+
+<text><textPath href="#MyPath">{props.medallion.name}</textPath></text>
+
+</svg>
+
+<div className="rounded-full self-center text-center h-full bg-yellow-400">
         <img src={Logo} alt="Princess" className="mx-auto self-center"></img>
         </div>
+        
         </div> 
         </div>
       )}
